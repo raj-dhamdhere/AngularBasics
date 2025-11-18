51f07549b809e2c8ff453ff441f86d678657df5e
Raj Dhamdhere <rajdhamdhere2112@gmail.com>
2025-11-18 10:53:09 +0000

    first checkout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserDetailComponent } from './users/user-detail/user-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'users/:id', component: UserDetailComponent },
+  // lazy route (we'll add file in step 6)
+
+  { path: '**', redirectTo: '' }
+];
